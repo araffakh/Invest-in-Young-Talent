2643053a5cf769d91366ada34719e67f15297cb2
aref alkadi <arefalkade7@gmail.com>
2024-10-23 19:34:07 +0300

    task1 completed

--- Aref's_Side_Tasks/task1/backend/main.js
+++ Aref's_Side_Tasks/task1/backend/main.js
@@ -88,7 +88,7 @@ const createWindow = () => {
     });
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
 };
 
 app.whenReady().then(() => {
