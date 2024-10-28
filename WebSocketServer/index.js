b021f4a23dad0b3037665692ab163c62844f9c8c
Khaldon araffa <araffakh@gmail.com>
2024-10-28 14:29:22 +0100

    Refactor WebSocket server implementation

---
+++ WebSocketServer/index.js
@@ -0,0 +1,38 @@
+// server.js
+const { createServer } = require("http");
+const WebSocket = require("ws");
+
+const server = createServer();
+const WebSocketServer = new WebSocket.Server({ server: server });
+
+let joyStickClient = null;
+let arduinoClient = null;
+
+WebSocketServer.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        const data = JSON.parse(message);
+
+        // Identify client type
+        if (data.type === "joystick") {
+            console.log("Joystick connected");
+            joyStickClient = ws;
+        } else if (data.type === "arduino") {
+            console.log("Arduino connected");
+            arduinoClient = ws;
+        } else {
+            // Forward messages
+            if (ws === joyStickClient && arduinoClient) {
+                arduinoClient.send(data.payload);
+            } else if (ws === arduinoClient && joyStickClient) {
+                joyStickClient.send(data.payload);
+            }
+        }
+    });
+
+    ws.on("close", () => {
+        if (ws === joyStickClient) joyStickClient = null;
+        if (ws === arduinoClient) arduinoClient = null;
+    });
+});
+
+server.listen(4000, () => console.log("WebSocket Server running on port 4000"));
